95667d05b6f481bce5176b1c0f487088ac4c733d
Leticia567ds <leticiaamarallopes8@gmail.com>
2023-04-17 10:13:30 -0300

    s

--- PRISMA/ContratePro/Front/Perfil/index.js
+++ PRISMA/ContratePro/Front/Perfil/index.js
@@ -1,39 +1,12 @@
-var UrList = "http://localhost:3000/frota";
-var itemFro = document.querySelector(".info");
-var listar = document.querySelector(".main");
-
-var listar = [];
-
 function carregar() {
-    const options = {
-        method: "GET"
-    }
-        fetch(UrList, options)
-        .then(res => res.json())
-        .then(res => {
-            listar = res;
-            lista();
-        })
-        .catch(err => console.error(err));
-    }
-
-
-function lista()  {
-    listar.forEach((e,i) => {
-        let frota = document.querySelector(".info").cloneNode(true);
-        frota.classList.remove("model");
-
-        frota.querySelector("#id").innerHTML += e.id;
-        frota.querySelector("#placa").innerHTML += e.placa;
-        frota.querySelector("#modelo").innerHTML += e.modelo;
-        frota.querySelector("#marca").innerHTML += e.marca;
-
-        frota.querySelector("#edit").setAttribute("onclick", `dadosAlterar('${i}')`);
-
-        frota.querySelector("#del").addEventListener("click", () => {
-            remover(e.id);
-        });
-
-        listarFro.appendChild(frota);
+  fetch("http://localhost:3000/prof")
+    .then((resp) => {
+      return resp.json();
     })
+
+    .then((data) => {
+      data.forEach((prof) => {
+        document.querySelector("#nome").innerHTML = prof.nome_p;
+      });
+    });
 }
