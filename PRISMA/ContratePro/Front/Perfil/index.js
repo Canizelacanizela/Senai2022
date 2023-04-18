5e70ad5c8f7345f1099cdda4e0b714fee4b60203
Leticia567ds <leticiaamarallopes8@gmail.com>
2023-04-18 10:49:24 -0300

    s

--- PRISMA/ContratePro/Front/Perfil/index.js
+++ PRISMA/ContratePro/Front/Perfil/index.js
@@ -1,12 +1,20 @@
 function carregar() {
-  fetch("http://localhost:3000/prof")
-    .then((resp) => {
-      return resp.json();
-    })
+  fetch('http://localhost:3000/prof')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(data => {
+      document.querySelector("#nome").innerHTML = data.nome_p;
+      document.querySelector("#biografia").innerHTML = data.biografia;
+      document.querySelector("#des").innerHTML = data.descricao;
+      document.querySelector("#cat").innerHTML = data.categorias.nome_categoria;
+      document.querySelector("#tel").innerHTML = data.tel;
+      document.querySelector("#email").innerHTML = data.email;
+      document.querySelector("#hora").innerHTML = data.preco_h;
 
-    .then((data) => {
-      data.forEach((prof) => {
-        document.querySelector("#nome").innerHTML = prof.nome_p;
-      });
     });
+    
+  })
+  .catch(error => {
+    console.error('Erro ao buscar dados:', error);
+  });
 }
