bd6ce42ad85ab74bb16be670a80cf6bd80d414a3
Canizelacanizela <felipecanizela@outlook.com>
2023-04-25 09:19:59 -0300

    correcao

--- PRISMA/ContratePro/back/prisma/migrations/20230425112226_muitopro/migration.sql
+++ PRISMA/ContratePro/back/prisma/migrations/20230425112226_muitopro/migration.sql
@@ -1,40 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `avalia` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `clientes` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `tabelaprofissionais` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `id_cliente` to the `Propostas` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropForeignKey
-ALTER TABLE `avalia` DROP FOREIGN KEY `Avalia_id_cliente_fkey`;
-
--- DropForeignKey
-ALTER TABLE `avalia` DROP FOREIGN KEY `Avalia_id_profissional_fkey`;
-
--- DropForeignKey
-ALTER TABLE `propostas` DROP FOREIGN KEY `Propostas_id_profissional_fkey`;
-
--- DropForeignKey
-ALTER TABLE `tabelaprofissionais` DROP FOREIGN KEY `tabelaProfissionais_id_categoria_fkey`;
-
--- AlterTable
-ALTER TABLE `propostas` ADD COLUMN `id_cliente` INTEGER NOT NULL;
-
--- DropTable
-DROP TABLE `avalia`;
-
--- DropTable
-DROP TABLE `clientes`;
-
--- DropTable
-DROP TABLE `tabelaprofissionais`;
-
--- DropTable
-DROP TABLE `user`;
-
 -- CreateTable
 CREATE TABLE `UserCliente` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -56,6 +19,15 @@ CREATE TABLE `UserProfissionais` (
     `porhora` VARCHAR(191) NOT NULL,
     `telefone` VARCHAR(191) NOT NULL,
     `id_categoria` INTEGER NOT NULL,
+    `id_coment` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `categoriaProfissoes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome_categoria` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -70,8 +42,22 @@ CREATE TABLE `avaliaProfisional` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Propostas` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `proposta` VARCHAR(191) NOT NULL,
+    `id_cliente` INTEGER NOT NULL,
+    `id_profissional` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `UserProfissionais` ADD CONSTRAINT `UserProfissionais_id_categoria_fkey` FOREIGN KEY (`id_categoria`) REFERENCES `categoriaProfissoes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserProfissionais` ADD CONSTRAINT `UserProfissionais_id_coment_fkey` FOREIGN KEY (`id_coment`) REFERENCES `avaliaProfisional`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `avaliaProfisional` ADD CONSTRAINT `avaliaProfisional_id_cliente_fkey` FOREIGN KEY (`id_cliente`) REFERENCES `UserCliente`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- PRISMA/ContratePro/back/src/controller/UserCli.js
+++ PRISMA/ContratePro/back/src/controller/UserCli.js
@@ -4,68 +4,67 @@ const jwt = require("jsonwebtoken");
 const prisma = new PrismaClient();
 
 const create = async (req, res) => {
-  let user = await prisma.user.create({
+  let userCliente = await prisma.userCliente.create({
     data: req.body,
   });
 
-  res.status(200).json(user).end();
+  res.status(200).json(userCliente).end();
 };
 
 const readOne = async (req, res) => {
-  let user = await prisma.user.findUnique({
+  let userCliente = await prisma.userCliente.findUnique({
     where: {
       id: Number(req.params.id),
     },
     select: {
       nome: true,
       email: true,
-      tipo: true,
+      telefone: true,
     },
   });
 
-  //SELECT * FROM user INNER JOIN publicacao ON user.id = publicacao.usuario_id WHERE ....
+  //SELECT * FROM userCliente INNER JOIN publicacao ON userCliente.id = publicacao.usuario_id WHERE ....
 
-  res.status(200).json(user).end();
+  res.status(200).json(userCliente).end();
 };
 
 const read = async (req, res) => {
-  let user = await prisma.user.findMany({
+  let userCliente = await prisma.userCliente.findMany({
     select: {
       id: true,
       email: true,
       nome: true,
       senha: true,
-      tipo: true,
+      telefone: true,
     },
   });
 
-  //SELECT email, nome FROM user WHERE email = ''
+  //SELECT email, nome FROM userCliente WHERE email = ''
 
-  res.status(200).json(user).end();
+  res.status(200).json(userCliente).end();
 };
 
 const login = async (req, res) => {
-  let user = await prisma.user.findMany({
+  let userCliente = await prisma.userCliente.findMany({
     where: {
       AND: [{ email: req.body.email }, { senha: req.body.senha }],
     },
     select: {
       id: true,
-      nome: true,
-      tipo: true,
+      nome: true
     },
   });
-  console.log(user[0]);
-  if (user[0] != undefined) {
+  console.log(userCliente[0]);
+  if (userCliente[0] != undefined) {
     jwt.sign(
-      user[0],
+      userCliente[0],
       process.env.KEY,
       { expiresIn: "10h" },
       function (err, token) {
         console.log(err);
         if (err == null) {
-          user[0]["token"] = token;
-          res.status(200).json(user[0]).end();
+          userCliente[0]["token"] = token;
+          res.status(200).json(userCliente[0]).end();
         } else {
           res.status(404).json(err).end();
         }
@@ -74,21 +73,21 @@ const login = async (req, res) => {
   } else {
     res.status(404).json({ msg: "Usuario ou Senha Invalidos" }).end();
   }
-  //SELECT id, nome FROM user WHERE email = '' AND senha = ''
+  //SELECT id, nome FROM userCliente WHERE email = '' AND senha = ''
 };
 
 const update = async (req, res) => {
-  const user = await prisma.user.update({
+  const userCliente = await prisma.userCliente.update({
     where: {
       id: Number(req.params.id),
     },
     data: req.body,
   });
-  res.status(202).json(user).end();
+  res.status(202).json(userCliente).end();
 };
 
 const del = async (req, res) => {
-  const user = await prisma.user.delete({
+  const userCliente = await prisma.userCliente.delete({
     where: {
       id: Number(req.params.id),
     },
